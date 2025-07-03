424ef071f2c6e53f4d632d8cce5071f3257ba8ba
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-07-02 22:13:50 -0400

    metadata

---
+++ spankbang.disablePreviews.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name          SpankBang - Disable Thumbnail Previews
+// @description   Stops thumbnails from running previews, unless the user hovers over the thumbnail.
+// @author        VoltronicAcid
+// @version       0.0.1
+// @homepageURL   https://github.com/VoltronicAcid/spankbangDisableThumbnailPreview
+// @supportURL    https://github.com/VoltronicAcid/spankbangDisableThumbnailPreview/issues
+// @match         http*://*spankbang.com/*
+// @run-at        document-idle 
+// ==/UserScript==
