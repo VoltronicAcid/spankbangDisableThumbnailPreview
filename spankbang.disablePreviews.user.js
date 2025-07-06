91311991de7746925c8823182fbb4da209739547
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-07-06 15:32:47 -0400

    querySelector refactor

--- spankbang.disablePreviews.user.js
+++ spankbang.disablePreviews.user.js
@@ -2,7 +2,7 @@
 // @name          SpankBang - Disable Thumbnail Previews
 // @description   Stops thumbnails from running previews, unless the user hovers over the thumbnail.
 // @author        VoltronicAcid
-// @version       0.0.1
+// @version       0.0.2
 // @homepageURL   https://github.com/VoltronicAcid/spankbangDisableThumbnailPreview
 // @supportURL    https://github.com/VoltronicAcid/spankbangDisableThumbnailPreview/issues
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=spankbang.com
@@ -10,39 +10,45 @@
 // @run-at        document-end 
 // ==/UserScript==
 
-if (document.location.pathname === "/") {
-    const mutationFn = (mutations) => {
-        mutations
-            .filter(({ target }) => target.tagName === "VIDEO" && target.src)
-            .forEach(mutation => {
-                const { target } = mutation;
-                const { parentElement: parent } = target;
+document.querySelectorAll(".js-media-list")
+    .forEach(list => {
+        const mutationFn = (mutations, observer) => {
+            mutations
+                .forEach(mutation => {
+                    const { target } = mutation;
+                    if (target.tagName !== "VIDEO" || !target.src) return;
+                    const { parentElement: parent } = target;
 
-                if (!parent.matches(":hover")) target.style.display = "none";
-            });
-    };
-    const observer = new MutationObserver(mutationFn);
-    document.querySelectorAll("a[x-data='videoItem']").forEach(div => {
-        observer.observe(div, { attributes: true, attributeFilter: ['style'], childList: true, subtree: true, });
-    });
-} else {
-    const mutationFn = (mutations, observer) => {
-        mutations
-            .forEach(mutation => {
-                const { target } = mutation;
-                const [videoDiv] = target.getElementsByClassName("video-js");
+                    if (!parent.matches(":hover")) {
+                        target.style.display = "none";
+                        observer.takeRecords();
+                    }
+                });
+        };
+        const observer = new MutationObserver(mutationFn);
 
-                if (videoDiv && mutation.oldValue === "video-item" && !target.matches(":hover")) {
-                    videoDiv.style.display = "none";
-                    target.classList.toggle("is_rotating", false);
-
-                    observer.takeRecords();
-                }
-            });
-    };
-    const observer = new MutationObserver(mutationFn);
-    Array.from(document.querySelectorAll("div.video-item"))
-        .forEach((div) => {
-            observer.observe(div, { attributes: true, attributeFilter: ['class'], attributeOldValue: true, });
+        list.querySelectorAll("a[x-data='videoItem']").forEach(link => {
+            observer.observe(link, { attributes: true, attributeFilter: ['style'], childList: true, subtree: true, });
         });
-}
+    });
+
+document.querySelectorAll("div.video-item")
+    .forEach((div) => {
+        const mutationFn = (mutations, observer) => {
+            mutations
+                .forEach(mutation => {
+                    const { target } = mutation;
+                    const [videoDiv] = target.getElementsByClassName("video-js");
+
+                    if (videoDiv && mutation.oldValue === "video-item" && !target.matches(":hover")) {
+                        videoDiv.style.display = "none";
+                        target.classList.toggle("is_rotating", false);
+
+                        observer.takeRecords();
+                    }
+                });
+        };
+        const observer = new MutationObserver(mutationFn);
+
+        observer.observe(div, { attributes: true, attributeFilter: ['class'], attributeOldValue: true, });
+    });
